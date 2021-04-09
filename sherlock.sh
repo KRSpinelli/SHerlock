f7c8d940628b559cee3c6fd0e8bfc516c5d2cf7f
Christian L Hardy <hardyc932@gmail.com>
2021-04-09 19:16:06 -0400

    option to continue if ping fails

--- sherlock.sh
+++ sherlock.sh
@@ -7,18 +7,25 @@ echo "Attempting scan on $1"
 echo "Checking connectivity..."
 
 mkdir $1
+cd $1
 
 # Use ping to check  if host is reachable
-ping -c 4 $1 > $1/$1-ping.log
+ping -c 4 $1 > $1-ping.log
 
-ANS=`grep -i "transmitted" $1/$1-ping.log | awk '{if ($4>0) print "true"; else print "false"}'`
+ANS=`grep -i "transmitted" $1-ping.log | awk '{if ($4>0) print "true"; else print "false"}'`
 
 if [ $ANS == 'true' ]
 then
 	echo "Connection successful!"
 else
 	echo "Host is either unreachable or blocking ICMP requests :("
-	exit 0
+	read -p "do you want to continue anyway? (y/n)" cont
+	if [ $cont == 'y' ]
+	then
+		:
+	else
+		exit 0
+	fi
 fi
 
 echo "Beginning nmap to discover open ports..."
@@ -29,9 +36,9 @@ echo "Ports discovered: $ports"
 echo "..."
 echo "Performing detailed scan of open ports..."
 
-nmap -Pn -oA "$1//$1" -sC -sV -p$ports $1
+nmap -Pn -oA "$1" -sC -sV -p$ports $1
 
-https=$(cat $1/$1.nmap | grep http | grep tcp | awk '{print $1}' | sed 's/\/tcp$//' | tr '\n' ',' | sed s/,$//)
+https=$(cat $1.nmap | grep http | grep tcp | awk '{print $1}' | sed 's/\/tcp$//' | tr '\n' ',' | sed s/,$//)
 
 echo "..."
 echo "Ports running http(s): $https"
@@ -43,9 +50,9 @@ IFS=',' read -r -a weblist <<< "$https"
 for FOO in "${weblist[@]}"
 do
 	echo "Conducting directory sweep of http://$1:$FOO"
-	mkdir $1/$FOO
-	gobuster dir -w /usr/share/wordlists/dirb/big.txt -u http://$1:$FOO/ -k -q -o $1/$FOO/gobuster_results > /dev/null	
-	paths=$(grep -v "403" $1/$FOO/gobuster_results | awk '{print $1}' | tr '\n' ', ' | sed 's/,$/\n/')
+	mkdir $FOO
+	gobuster dir -w /usr/share/wordlists/dirb/big.txt -u http://$1:$FOO/ -k -q -o $FOO/gobuster_results > /dev/null	
+	paths=$(grep -v "403" $FOO/gobuster_results | awk '{print $1}' | tr '\n' ', ' | sed 's/,$/\n/')
 	echo "URL paths found: $paths"
 done
 
